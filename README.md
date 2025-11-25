1d3fe0dda447cd0dcf83d4d12da8b33f2022016a
Sean Pino <Seanpino@gmail.com>
2025-11-25 17:23:47 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Example
-An example greeter plugin
+# Salvaging Plugin
+A plugin designed to assist with afk, stats, and information for shipwreck salvaging
+
+## Authors
+- Pino (Pi no)
+- GIM Serenity
+
+## Changelog
+
+v1.0.0
+- Initial Release
--- src/test/java/com/salvaging/SalvagingPluginTest.java
+++ src/test/java/com/salvaging/SalvagingPluginTest.java
@@ -1,13 +1,15 @@
-package com.example;
+package com.salvaging;
+
+import com.salvaging.SalvagingPlugin;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class SalvagingPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(SalvagingPlugin.class);
 		RuneLite.main(args);
 	}
 }
